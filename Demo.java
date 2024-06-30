e61b216555e5bf7918299abc4e56b78b17c24c57
Rahul Alone <alonerahul23@gmail.com>
2024-06-30 16:05:59 +0530

    Update for feature branch

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
 public class Demo{
     public static void main(String args[])  //static method  
 {  
-System.out.println("Hello Good Morning");  
-System.out.println("Hello Good Morning  update 1");  
+    System.out.println("Hello Good Morning");  
+    System.out.println("Hello Good Morning  update 1");  
+    System.out.println("Feature branch update 2");  
 }  
 }
