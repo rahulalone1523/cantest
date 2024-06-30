716b7fe0a7b9b0bd9b1fe37001c157ed882a39da
Rahul Alone <alonerahul23@gmail.com>
2024-06-30 17:28:41 +0530

    Removed extra Sysouts!!!

--- Demo.java
+++ Demo.java
@@ -1,8 +1,7 @@
 public class Demo{
     public static void main(String args[])  //static method  
 {  
-    System.out.println("Hello Good Morning");  
-    System.out.println("Hello Good Morning  update 1");  
-    System.out.println("Feature branch update 2");  
+
+    System.out.println("Hello");
 }  
 }
