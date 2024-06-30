3743d3bfb21698d74a7111a8e3c0a385da91aaaf
Rahul Alone <alonerahul23@gmail.com>
2024-06-30 16:47:27 +0530

    New Test

--- Demo.java
+++ Demo.java
@@ -1,11 +1,16 @@
 public class Demo{
     public static void main(String args[])  //static method  
 {  
+<<<<<<< HEAD
     System.out.println("Hello Good Morning");  
 
     System.out.println("Revert push changes to repo!!");  
     
     System.out.println("Hello Good Morning  update 3");  
 
+=======
+System.out.println("Hello Good Morning");  
+System.out.println("Hello Good Morning  update 1");  
+>>>>>>> parent of 0ede3f5 (Added to test revert push)
 }  
 }
